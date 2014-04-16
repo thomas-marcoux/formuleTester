f4a6624b11644c03bc1c1817d510d7d7d5ebfadd
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-04-16 14:53:23 +0200

    fixed structure

--- src/structure.ml
+++ src/structure.ml
@@ -1,5 +1,5 @@
 (* Structure d'une formule propositionnelle polymorphe *)
-type 'a valeur_prop = True | False;;
+type valeur_prop = True | False;;
 
 
 type 'a formule_prop =    Ou of 'a formule_prop * 'a formule_prop	
@@ -7,5 +7,5 @@ type 'a formule_prop =    Ou of 'a formule_prop * 'a formule_prop
 			| Implique of 'a formule_prop * 'a formule_prop
 			| Negation of 'a formule_prop
 			| Equivalence of 'a formule_prop * 'a formule_prop
-			| Variable of 'a formule_prop
+			| Variable of string
 			| Valeur of valeur_prop;;
