3a4d4699cbb86a6dfde868775d4492240f4973f2
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-04-23 17:54:12 +0200

    Balayage corrige, main.ml mis a jour et recompile. Reference au Makefile supprime du README

--- src/main.ml
+++ src/main.ml
@@ -86,33 +86,7 @@ let rec reductionFormule f =
   let f2 = reductionEquivalences f in
   if (f2 = f) then f2 else reductionFormule f2;;
 
-
-
-
-(*********************************
-********* Balayage.ml ************
-**********************************
-
-et f = Implique (Implique (Variable "p", Variable "q"), Variable "p");;
-let rec eval = fun f1
-  -> let f2 = reductionEquivalences f1
-     in if f2 = f1 then f2 else eval f2;;
-let balayage = fun f1
-  -> let f = elimination f1
-     in let rec aux = fun l
-	  -> match l with
-	       [] -> false
-	     | h::t -> let f2 = eval (Et (attributionValeur f (fst h) True, attributionValeur f (fst h) False))
-		       in if f2 = Valeur True then true
-			  else if eval (Negation (f2)) = Valeur True
-			  then true
-			  else aux (suppVariable l h)
-	in aux (listeVariables f);;
-  balayage f;;
-eval (elimination f);;
-
-
-*************************************
+(*************************************
 ********* Satisfiabilite.ml **********
 **************************************)
 
@@ -202,7 +176,24 @@ let ensembleSatisfiabilite = function liste ->
     | h::t -> aux t (assocList@[((to_String h) ^ (lectu (satisfiabilite h) ^ " // "))])
   in aux liste [];;
 
+(*********************************
+********* Balayage.ml ************
+**********************************)
 
+let balayage = fun f1
+  -> let f = elimination f1
+     in let rec aux = fun l
+	  -> match l with
+	       [] -> false
+	     | h::t -> let f2 = reductionFormule
+				  (Et (attributionValeur f (fst h) True,
+				       attributionValeur f (fst h) False))
+		       in if f2 = Valeur True then true
+			  else if reductionFormule
+				    (Negation (f2)) = Valeur True
+			  then true
+			  else aux t
+	in aux (listeVariables f);;
 
 (************************************
 ********* SaisieClavier.ml **********
